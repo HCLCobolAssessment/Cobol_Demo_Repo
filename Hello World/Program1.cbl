fa6f790fdb2046d400f49fad5d55fd06baf0e5ed
Administrator <Administrator@172.31.18.58>
2019-02-19 11:24:26 +0000

    Hello world program commit

---
+++ Hello World/Program1.cbl
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Program1.
+       AUTHOR.  HCL.
+       environment division.
+       configuration section.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.     
+       PROCEDURE DIVISION.
+       DISPLAY "HELLO hi bye bye".
+       goback.
+       STOP RUN.
+       end program Program1.
