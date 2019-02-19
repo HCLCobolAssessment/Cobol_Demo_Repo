160888c14c02df62f4600edf913d95a5cfb6352e
Administrator <Administrator@172.31.18.58>
2019-02-19 11:46:42 +0000

    message changed

--- Hello World/Program1.cbl
+++ Hello World/Program1.cbl
@@ -6,7 +6,7 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.     
        PROCEDURE DIVISION.
-       DISPLAY "HELLO ih bye bye".
+       DISPLAY "HELLO hi bye bye".
        goback.
        STOP RUN.
        end program Program1.
